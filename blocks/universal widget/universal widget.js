bbaa08d10d70d96e922a8f96c04bafed4eee8af2
hemanth-acc <hemanth.vemulapalli@accenture.com>
2025-11-18 12:29:18 +0530

    Experiment-2

--- blocks/universal widget/universal widget.js
+++ blocks/universal widget/universal widget.js
@@ -2,5 +2,7 @@ export default function decorate(block) {
     const flightSearch = document.createElement('div')
     flightSearch.id = 'flight-search';
     flightSearch.setAttribute('data-widget-type', 'flight-search');
+    flightSearch.innerHTML = 'Hi'
     block.replaceChildren(flightSearch);
+
 }
